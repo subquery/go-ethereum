5c416d7370414aa2aa82f3b402ba0c2ea0e8975d
Scott Twiname <skott.twiname@gmail.com>
2024-04-03 16:21:29 +1300

    Revert limit change

--- eth/filters/filter.go
+++ eth/filters/filter.go
@@ -197,7 +197,7 @@ func (f *Filter) Logs(ctx context.Context) ([]*types.Log, error) {
 			blocks[log.BlockNumber] = true
 		}
 
-		if len(blocks) >= int(f.limit) {
+		if len(blocks) > int(f.limit) {
 			return true
 		}
 
